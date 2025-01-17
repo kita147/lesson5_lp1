b748db42cb181884d16e507e636bdcbccb2108a3
kita147 <work.kitayama@gmail.com>
2025-01-17 17:32:59 +0900

    jQuery設置、ハンバーガーメニューとタブ実装

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,9 +1,20 @@
 /* globals $ */
 
+////// ハンバーガーメニュー //////
 $(document).ready(function () {
   $(".hamburger").on("click", function () {
-    // ハンバーガーメニューのアクティブ状態を切り替え
-    $(this).toggleClass("active");
-    $(".header__nav--sp").toggleClass("active");
+    $(this).toggleClass("active"); // hamburgerにactiveクラスを切り替える
+    $(".header__nav--sp").toggleClass("active"); // header__nav--spにactiveクラスを切り替える
+  });
+});
+
+////// タブメニュー //////
+$(document).ready(function () {
+  let tabs = $(".course__tab");
+  tabs.on("click", function () {
+    $(".active").removeClass("active");
+    $(this).addClass("active");
+    const index = tabs.index(this);
+    $(".course__content").removeClass("show").eq(index).addClass("show");
   });
 });
